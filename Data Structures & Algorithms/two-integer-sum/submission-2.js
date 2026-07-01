87ceba4340f7fc509a8041581eedd8dbc6d14420
Parth Kadam <parthkadamalt@gmail.com>
2026-07-01 14:17:45 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let mp = new Map()
+        for(let i=0;i<nums.length;i++){
+            let rem= target - nums[i]
+            if(mp.has(rem)) return [mp.get(rem),i]
+            mp.set(nums[i],i)
+        }
+    }
+}
